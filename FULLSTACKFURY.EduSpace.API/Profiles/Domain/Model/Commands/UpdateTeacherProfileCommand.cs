970a13918d003bc26f211fdac131177868bcabd6
vquijandria <u201822697@upc.edu.pe>
2025-11-13 23:32:21 -0500

    feat: added UpdateTeacherProfileCommand.cs

---
+++ FULLSTACKFURY.EduSpace.API/Profiles/Domain/Model/Commands/UpdateTeacherProfileCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.Profiles.Domain.Model.Commands;
+
+public record UpdateTeacherProfileCommand(
+    string TeacherId,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Dni,
+    string Address,
+    string Phone
+);
