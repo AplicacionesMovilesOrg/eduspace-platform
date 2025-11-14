53ea8002b13c7a76ef9d40e271126193340d69ce
vquijandria <u201822697@upc.edu.pe>
2025-11-13 23:32:34 -0500

    feat: added UpdateTeacherProfileCommandFromResourceAssembler.cs

---
+++ FULLSTACKFURY.EduSpace.API/Profiles/Interfaces/REST/Transform/UpdateTeacherProfileCommandFromResourceAssembler.cs
@@ -0,0 +1,22 @@
+﻿using FULLSTACKFURY.EduSpace.API.Profiles.Domain.Model.Commands;
+using FULLSTACKFURY.EduSpace.API.Profiles.Interfaces.REST.Resources;
+
+namespace FULLSTACKFURY.EduSpace.API.Profiles.Interfaces.REST.Transform;
+
+public static class UpdateTeacherProfileCommandFromResourceAssembler
+{
+    public static UpdateTeacherProfileCommand ToCommand(
+        string teacherId,
+        UpdateTeacherProfileResource resource)
+    {
+        return new UpdateTeacherProfileCommand(
+            TeacherId: teacherId,
+            FirstName: resource.FirstName,
+            LastName:  resource.LastName,
+            Email:     resource.Email,
+            Dni:       resource.Dni,
+            Address:   resource.Address,
+            Phone:     resource.Phone
+        );
+    }
+}
